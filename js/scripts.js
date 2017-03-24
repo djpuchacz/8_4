ad18a5b5c1d676c82215340eb78bf9c5e046eb0b
Łukasz Woźniak <ukasz Woniak>
2017-03-24 21:27:15 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = 5,
+    h = 4;
+    triangleArea = a*h/2;
+document.write('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
